a4a96645debc651c7f36b7382b97a189f3c12a59
davidmasp <david.mas.p@gmail.com>
2017-04-24 23:14:20 +0200

    adding oneletter redirections

--- newpage/newpage.js
+++ newpage/newpage.js
@@ -27,3 +27,16 @@ var curr_month = d.getMonth();
 var curr_year = d.getFullYear();
 document.getElementById('date').innerHTML = curr_date + "-" + m_names[curr_month] + "-" + curr_year;
 }
+
+function completeAndRedirect(){
+  var query = document.forms[0].elements[0].value;
+  if (query == "n") {
+    location.href='https://www.netflix.com/browse';
+  } else if (query.startsWith("d..")) {
+    var a = query.split("..");
+    var query = a[1]
+    location.href='https://duckduckgo.com/?q='+query;
+  } else {
+    location.href='https://www.google.es/search?q='+query;
+  }
+}
